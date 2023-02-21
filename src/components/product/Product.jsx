0c3a1a7e5fb141f0186d69db1417b665557517de
DevSinghParihar10 <dev-singh.parihar@capgemini.com>
2023-02-21 10:58:21 +0530

    button styling fix

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -46,10 +46,10 @@ const Product = ({product}) => {
         <h6><span style={{ fontSize: '12px'}}>Type</span> : {product.product.type}</h6>
         <h6><span style={{ fontSize: '12px'}}>Color</span> : {product.product.colour}</h6>
         <div className='btn-links'>
-          { customer.customer !== null ? <button className="btn btn-outline-danger" onClick={handleCart}>View</button> : <button  onClick={handleEdit}>Edit</button> }
+          { customer.customer !== null ? <button className="btn btn-outline-danger" onClick={handleCart}>View</button> : <button className="btn btn-outline-danger"  onClick={handleEdit}>Edit</button> }
         
         
-          <button onClick={addToCart}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-heart-fill" viewBox="0 0 16 16">
+          <button className="btn btn-outline-success btn-sm" onClick={addToCart}>Wishlist<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" className="bi bi-bag-heart-fill" viewBox="0 0 16 16">
   <path d="M11.5 4v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5ZM8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1Zm0 6.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
 </svg></button>
           
