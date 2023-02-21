eb5f0a7b1d65f3705b4eb5dc6161919c57fb2eda
DevSinghParihar10 <dev-singh.parihar@capgemini.com>
2023-02-21 12:43:40 +0530

    navbar fix

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -24,16 +24,35 @@ function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
-        <Link class="navbar-brand" to="/">SastaBasta</Link>
+       <div className="brand">
+        SastaBasta
+       </div>
         <div class="collapse navbar-collapse" id="navbarColor01">
           <ul class="navbar-nav me-auto">
            
-            <li class="nav-item">
-              <Link class="nav-link" to="/register">Register</Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/login">Login</Link>
-            </li>
+          {admin.admin == null && customer.customer == null ?
+              <li class="nav-item">{customer.customer == null ? <Link class="nav-link" to="/register">Register</Link> : <button >Hello</button>}
+              </li> : ''
+            }
+            
+            {/* login nav */}
+            {admin.admin == null && customer.customer == null ?
+              <li class="nav-item">{customer.customer == null ? <Link class="nav-link" to="/login">Login</Link> : <button >Hello</button>}
+              </li> : ''
+            }
+            
+            {/* admin dashboard link */}
+            {admin.admin != null && customer.customer == null ?
+              <li class="nav-item">{admin.admin != null ? <Link class="nav-link" to="/adminDash">Dashboard</Link> : <button >Hello</button>}
+              </li> : ''
+            }
+
+            {/* customer dashboard nav */}
+            {admin.admin == null && customer.customer != null ?
+              <li class="nav-item">{customer.customer != null ? <Link class="nav-link" to="/customerDash">Dashboard</Link> : <button >Hello</button>}
+              </li> : ''
+            }
+
             {admin.admin != null || customer.customer != null ?
               <li class="nav-item">
                 {admin.admin != null ? <button onClick={() => dispatch(AdminLogout())
